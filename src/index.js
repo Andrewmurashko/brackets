cbd9a01556891a16bd8b1a3f39351706d0626901
Andrewmurashko <70801670+Andrewmurashko@users.noreply.github.com>
2020-10-25 20:12:38 +0300

    feat: complete brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let array = [];
+    let k = str.length / 2;
+
+    while (k >= 0) {
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            array = bracketsConfig[i][0].concat(bracketsConfig[i][1]);
+            while (str.indexOf(array) != -1) {
+                str = str.replace(array, "");
+            }
+        }
+        k--;
+    }
+    return !str.length;
+};
